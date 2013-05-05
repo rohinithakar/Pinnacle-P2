b8ef84917dc947dcd63b81487423cf153a59e510
rohinithakar <rohini.thakar85@gmail.com>
2013-05-05 13:38:01 -0700

    Change to moocinstance model

--- djangobottle/djangobottle/app1/models.py
+++ djangobottle/djangobottle/app1/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class MoocInstance(models.Model):
     team_name = models.CharField(max_length=50)
-    url = models.CharField(max_length=50)
+    url = models.CharField(max_length=200)
     default = models.BooleanField()
